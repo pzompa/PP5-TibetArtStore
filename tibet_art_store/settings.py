d9755e9d24dd842d83d437c47ea8a08fc16b0c3e
pzompa <>
2023-08-28 10:31:03 +0000

    Create home app with view and template for homepage.

--- tibet_art_store/settings.py
+++ tibet_art_store/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -68,7 +69,10 @@ ROOT_URLCONF = 'tibet_art_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
