c7998a625a36877dfb29ceaf17b0276b94554363
Pema Zompa <pemazompa@hotmail.com>
2023-09-02 22:24:42 +0000

    Register products and category models with admin

---
+++ products/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Product, Category
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'sku', 'productCategory']
+    search_fields = ['title', 'sku']
+    list_filter = ['productCategory']
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
