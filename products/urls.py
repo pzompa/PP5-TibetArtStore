a4aae8ca5e95d46af723e606cdc3218780a2b085
Pema Zompa <pemazompa@hotmail.com>
2023-09-04 07:43:42 +0000

    Add view, url and template for product list with filter and sort function

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 
 urlpatterns = [
-    path('<int:product_id>/detail/', views.product_detail, name='product_detail'),
-    path('', views.products_list, name='products_list')
+    path('', views.products_list_filter, name='products_list'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,23 +1,56 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Product
+from .models import Product, Category
+from .forms import ProductForm
+from django.db.models import Q
 
 
-def products_list(request):
-    """ view to display all products"""
+def products_list_filter(request):
+    # GET DATA from DB
+    category = request.GET.get('category')
+    price_range = request.GET.get('price_range')
+    sorting = request.GET.get('sorting')
 
     products = Product.objects.all()
-    context = {
-        'products': products
+
+    # Filter DATA
+    if category and category != 'all':
+        products = products.filter(productCategory_id=category)
+    if price_range == '<100':
+        products = products.filter(price__lt=100)
+    elif price_range == '<1000':
+        products = products.filter(price__lt=1000)
+    elif price_range == '<5000':
+        products = products.filter(price__lt=5000)
+
+    # Sort DATA
+    sort_map = {
+        'name_asc': 'title',
+        'name_desc': '-title',
+        'price_asc': 'price',
+        'price_desc': '-price',
     }
+    sort_key = sort_map.get(sorting, 'title') 
+    products = products.order_by(sort_key)
+
+    # remove full/ string
+    modified_products = []
+    
+    for product in products:
+        image_name = str(product.productImageName)
+        if image_name.startswith('full/'):
+            print(f"Stripping 'full/' from {image_name}")
+            image_name = image_name[5:]
+
+        modified_products.append({
+            'product': product,
+            'image_name': image_name
+        })
+
+    context = {
+        'products': modified_products,
+        'category': category,
+        'price_range': price_range,
+        'sorting': sorting
+    }
+
     return render(request, 'products/products_list.html', context)
-
-
-def product_detail(request, product_id):
-    """ view to display all products"""
-
-    product = get_object_or_404(Product, id=product_id)
-    context = {
-        'product': product
-    }
-    return render(request, 'products/product_detail.html', context)
-
