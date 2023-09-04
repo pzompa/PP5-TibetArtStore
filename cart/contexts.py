d7feb5cde3e61c260b9eb2c0f4d54e19158ae1b0
Pema Zompa <pemazompa@hotmail.com>
2023-09-04 18:37:41 +0000

    Updated context.py file and cart template

--- cart/contexts.py
+++ cart/contexts.py
@@ -4,9 +4,6 @@ from django.shortcuts import get_object_or_404
 from products.models import Product
 # other imports
 
-# the rest of your file
-
-
 def cart_contents(request):
     """ 
     Permits other apps to access cart contents 
@@ -18,27 +15,15 @@ def cart_contents(request):
     cart = request.session.get('cart', {})
 
     for item_id, item_data in cart.items():
-        if isinstance(item_data, int):
-            product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
-            item_count += item_data
-            cart_items.append({
-                'item_id': item_id,
-                'quantity': item_data,
-                'product': product,
-            })
+        product = get_object_or_404(Product, pk=item_id)
+        total += item_data * product.price
+        item_count += item_data
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': item_data,
+            'product': product,
+        })
 
-        else:
-            product = get_object_or_404(Product, pk=item_id)
-            for size, quantity in item_data['items_by_size'].items():
-                total += quantity * product.price
-                product_count += quantity
-                cart_items.append({
-                    'item_id': item_id,
-                    'quantity': quantity,
-                    'product': product,
-                    'size': size,
-                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
