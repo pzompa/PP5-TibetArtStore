7ac3eaed474dddd11902f73e6e7eecec04c6100a
Pema Zompa <pemazompa@hotmail.com>
2023-09-04 17:35:42 +0000

    Created add to cart functionality

---
+++ cart/contexts.py
@@ -0,0 +1,62 @@
+from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+# other imports
+
+# the rest of your file
+
+
+def cart_contents(request):
+    """ 
+    Permits other apps to access cart contents 
+    """
+    
+    cart_items = []
+    total = 0
+    item_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, item_data in cart.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            item_count += item_data
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                cart_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
+
+    context = {
+        'cart_items' : cart_items,
+        'total': total,
+        'item_count': item_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
+    }
+
+    return context
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
 
 # Create your views here.
 
@@ -7,3 +8,24 @@ def view_cart(request):
     """ A view to return the bag page """
 
     return render(request, 'cart/cart.html')
+
+def add_to_cart(request, product_id):
+    """ Add a quantity of the specified product to the shopping bag """
+    
+    cart = request.session.get('cart', {})
+    
+    try:
+        quantity = int(request.POST.get('quantity'))
+        cart[product_id] = cart.get(product_id, 0) + quantity
+    except TypeError:
+        quantity = 1
+        cart[product_id] = cart.get(product_id, 0) + quantity
+    
+    redirect_url = request.POST.get('redirect_url', '/')
+    
+    request.session['cart'] = cart
+    print(request.session['cart'])
+
+    return redirect(redirect_url)
+
+
