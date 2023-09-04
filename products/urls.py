4bac473be9f822d9d13115957efe097ad156bc3e
Pema Zompa <pemazompa@hotmail.com>
2023-09-04 07:59:40 +0000

    Add view, url and template for mandala list

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.products_list_filter, name='products_list'),
     path('thanka-paintings-list/', views.thanka_paintings_view, name='thanka_paintings_list'),
     path('<int:product_id>/detail/', views.product_detail, name='product_detail'),
+    path('mandala-list/', views.mandala_view, name='mandala_list'),
 ]
