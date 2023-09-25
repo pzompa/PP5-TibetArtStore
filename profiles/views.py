07c421ebda4b4ce82479e130a58084b724f8eaed
Pema Zompa <pemazompa@hotmail.com>
2023-09-25 21:51:38 +0000

    Update profile views, template and css

--- profiles/views.py
+++ profiles/views.py
@@ -1,27 +1,31 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
 
 @login_required
 def profile(request):
-    """Display the user's profile."""
-    profile = request.user.userprofile
-    return render(request, 'profiles/display_profile.html', {'profile': profile})
-
-@login_required
-def update_profile(request):
-    """Update the user's profile."""
+    """Display and update the user's profile."""
     profile = get_object_or_404(UserProfile, user=request.user)
-    
+
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Profile updated successfully')
             return redirect(reverse('profile'))
     else:
         form = UserProfileForm(instance=profile)
+        orders = profile.orders.all()
 
-    return render(request, 'profiles/update_profile.html', {'form': form})
+        template = 'profiles/profile.html'
+        context = {
+            'form': form,
+            'orders': orders,
+            'on_profile_page':True
+        }
+
+        return render(request, template, context)
