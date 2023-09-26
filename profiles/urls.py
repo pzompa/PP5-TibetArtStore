210782d0bd17320ef0463bf5e90bc9bbfd38984e
Pema Zompa <pemazompa@hotmail.com>
2023-09-26 13:13:54 +0000

    Added order history and updated checkout views.

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('profile/update/', views.update_profile, name='update_profile'),
+    # path('profile/update/', views.update_profile, name='update_profile'),
+    path('order_history/<order_number>/', views.order_history, name='order_history')
+
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -29,3 +29,19 @@ def profile(request):
         }
 
         return render(request, template, context)
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is your past confirmation for order_number{ order_number}.'
+        'A confirmation email was sent on the{ order_date }.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
