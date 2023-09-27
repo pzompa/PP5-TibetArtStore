a9de564192bf590959b7059fec5f5765903a8797
Pema Zompa <pemazompa@hotmail.com>
2023-09-27 12:21:45 +0000

    Updated webhook handler to handle profiles

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -4,6 +4,7 @@ from products.models import Product
 from django.template.loader import render_to_string
 from django.core.mail import send_mail
 from django.conf import settings
+from profiles.models import UserProfile
 
 import json
 import time
@@ -49,7 +50,6 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded event from Stripe.
         """
-        print('now we entered the def zone')
         intent = event.data.object
         pid = intent.id
         cart = intent.metadata.cart
@@ -68,6 +68,23 @@ class StripeWH_Handler:
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
+            
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = (
+                    shipping_details.address.line1)
+                profile.default_street_address2 = (
+                    shipping_details.address.line2)
+                profile.default_county = shipping_details.address.state
+                profile.save()
 
         order_exists = False
         attempt = 1
@@ -101,6 +118,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
