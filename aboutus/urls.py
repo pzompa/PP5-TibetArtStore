8cc2d1544fc41eaec648685d6f9e5f7b67db6c99
Pema Zompa <pemazompa@hotmail.com>
2023-09-28 09:50:20 +0000

    add views and urls for policys of the site

--- aboutus/urls.py
+++ aboutus/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.about_us, name='about_us'),
+    path('aboutus/', views.service_policy, name='service_policy'),
+    path('aboutus/', views.privacy_policy, name='privacy_policy')
+
 ]
--- aboutus/views.py
+++ aboutus/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def about_us(request):
     return render(request, 'aboutus/about_us.html')
+
+def service_policy(request):
+    return render(request, 'aboutus/service_policy.html')
+
+def privacy_policy(request):
+    return render(request, 'aboutus/privacy_policy.html')
