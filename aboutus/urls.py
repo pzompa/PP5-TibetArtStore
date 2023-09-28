f61aca3a63553e1a2508845116c7e9e93dd8ac0e
Pema Zompa <pemazompa@hotmail.com>
2023-09-28 12:22:52 +0000

    Create About us page and updated url in footer.html

--- aboutus/urls.py
+++ aboutus/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.about_us, name='about_us'),
-    path('aboutus/', views.service_policy, name='service_policy'),
-    path('aboutus/', views.privacy_policy, name='privacy_policy')
+    path('service_policy/', views.service_policy, name='service_policy'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy')
 
 ]
