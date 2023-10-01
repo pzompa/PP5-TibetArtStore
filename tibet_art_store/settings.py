04e50decd7baa4540926308d71db0955fb5f1718
Pema Zompa <pemazompa@hotmail.com>
2023-10-01 15:14:32 +0000

    Add Elephant DB, Update Settings.py for Heroku Deployment

--- tibet_art_store/settings.py
+++ tibet_art_store/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 if os.path.isfile('env.py'):
     import env
@@ -34,7 +35,8 @@ ALLOWED_HOSTS = [
     '8000-pzompa-pp5-tibet-art-sto-o7rba25jeg.us2.codeanyapp.com',
     'tibetartstore-0faa2ebe064c.herokuapp.com',
     '8002-pzompa-pp5-tibet-art-sto-o7rba25jeg.us2.codeanyapp.com',
-    '8000-pzompa-pp5tibetartstore-zz9u3ctg3dl.ws-eu104.gitpod.io'
+    '8000-pzompa-pp5tibetartstore-zz9u3ctg3dl.ws-eu104.gitpod.io',
+    '8000-pzompa-pp5tibetartstore-gplv5kfei63.ws-eu105.gitpod.io',
 ]
 
 
@@ -136,13 +138,18 @@ WSGI_APPLICATION = 'tibet_art_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
