7d7bc3ce05162a0ae50de506c16fd7c6e1f22931
Pema Zompa <pemazompa@hotmail.com>
2023-10-01 19:06:59 +0000

    Add Newsletter subscription functionality

--- contact/views.py
+++ contact/views.py
@@ -5,10 +5,11 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.contrib import messages
 from datetime import datetime
+from django.template.loader import render_to_string
 
 
 def contact_us(request):
-    form = ContactForm()  # Create a new instance of the form
+    form = ContactForm() 
     return render(request, 'contact/contact_us.html', {'form': form})
 
 
@@ -41,10 +42,23 @@ def contact_view(request):
                 fail_silently=False,
                 html_message=message
             )
-            # Or wherever you want to redirect after successful submission
             return render(request, 'contact/thank_you_contact.html', {'name': instance.name, 'comment': instance.comment})
     else:
         form = ContactForm()
 
     return render(request, 'contact/contact_form.html', {'form': form})
 
+
+def newsletter(request):
+    if request.method == 'POST':
+        subscriber_email = request.POST.get('email')
+        subject = 'Welcome to Tibet Art Shop Newsletter!'
+
+        body = render_to_string('email_templates/subscription_email.txt')
+
+        send_mail(subject, body, 'newsletter-service@tibetartberlin.com', [subscriber_email])
+        messages.info(request, 'Thank you for subscribing to our Newsletter')
+
+        return redirect('home')
+
+    return redirect('home')
