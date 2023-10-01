4db4ece3c0c79380aa220dfb1019c782110fa2e4
Pema Zompa <pemazompa@hotmail.com>
2023-10-01 19:08:20 +0000

    Update Media Url path

--- contact/urls.py
+++ contact/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.contact_us, name='contact_us'),
     path('contact-us/', views.contact_view, name='contact_view'),
+    path('newsletter/', views.newsletter, name='newsletter')
 ]
