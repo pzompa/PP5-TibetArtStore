5f475fbd47d90b1b1979da9ce95eb986425e4b87
Pema Zompa <pemazompa@hotmail.com>
2023-10-05 10:43:44 +0000

    Fix btn responsive issues and update 403 view.

--- tibet_art_store/views.py
+++ tibet_art_store/views.py
@@ -7,5 +7,6 @@ def handler404(request, exception):
 def handler500(request):
     return render(request, "errors/500.html", status=500)
 
-def handler403(request):
+def handler403(request, exception):
+    """Error Handler 403 - Forbidden"""
     return render(request, "errors/403.html", status=403)
